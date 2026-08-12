aab185c785794689e41ddd36f46534df638ab9ae
hsLeecoder <dlgotjd2003@gmail.com>
2026-08-12 20:37:12 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/todobackend/TestController.java
@@ -0,0 +1,13 @@
+package com.example.todobackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "환경설정 통일 확인";
+    }
+}

dd69c762190a419dd3c73f922be875245bae6bb4
shlee <userlsh111@gmail.com>
2026-08-25 17:58:53 +0900

    toSubmit_mid

---
+++ create database todo_db.sql
@@ -0,0 +1,7 @@
+-- CREATE DATABASE newTodo DEFAULT CHARACTER
+-- SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- -- create 할때만. 
+
+-- CREATE DATABASE todo_db;
+use todo_db;
+alter DATABASE todo_db CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
